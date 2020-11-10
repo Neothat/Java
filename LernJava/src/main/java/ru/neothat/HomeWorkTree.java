2ca0f22fa57ee84d119f8164a92353d69bccbc2b
neothat <neothat@mail.ru>
2020-11-10 05:53:24 +0300

    work on bugs

--- LernJava/src/main/java/ru/neothat/HomeWorkTree.java
+++ LernJava/src/main/java/ru/neothat/HomeWorkTree.java
@@ -5,7 +5,7 @@ import java.util.Scanner;
 
 public class HomeWorkTree {
 
-    public static int userAnswer;
+    /*public static int userAnswer;
     public static Scanner in = new Scanner(System.in);
     public static int tryCount = 3;
 
@@ -52,6 +52,60 @@ public class HomeWorkTree {
 
         userAnswer = in.nextInt();
         in.nextLine();
+    }*/
+
+    public static int userAnswer;
+    public static Scanner in = new Scanner(System.in);
+    private static int answer;
+
+    public static void main(String[] args) {
+        do {
+            int tryCount = 3;
+
+            Random random = new Random();
+            answer = random.nextInt(10);
+
+            System.out.println("Добро пожаловать в игру угадайка!" +
+                    "\nПопробуйте угадать число от 0 до 9 за 3 хода");
+            for (; tryCount > 0; tryCount--) {
+                do {
+                    System.out.println("Введите число от 0 до 9. У вас осталось " + tryCount +" попытка(ки)");
+                    userAnswer = in.nextInt();
+                    in.nextLine();
+                }while (userAnswerValid(userAnswer));
+
+                if (userAnswer == answer){
+                    System.out.println("Вы победили!");
+                    break;
+                }
+                checkLessMore(userAnswer);
+            }
+            System.out.println("Вы проиграли. Правильный ответ: " + answer);
+        }while (restart());
+
     }
+
+    private static boolean restart() {
+        while (true){
+            System.out.println("Хотите попробывать еще раз? y/n");
+            String rerun = in.nextLine();
+            if (rerun.equals("y")) return true;
+            if (rerun.equals("n")) return false;
+        }
+    }
+
+    private static void checkLessMore(int userAnswer) {
+        if (userAnswer > answer){
+            System.out.println("Вы ввели слишком большое число");
+        } else {
+            System.out.println("Вы ввели слишком маленькое число");
+        }
+    }
+
+    private static boolean userAnswerValid(int userAnswer) {
+        return userAnswer < 0 || userAnswer > 9;
+    }
+
+
 }
 
