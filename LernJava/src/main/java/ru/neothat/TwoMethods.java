135befcbcf06a0bf25aab5ce311d6b5ae5f9edd6
neothat <neothat@mail.ru>
2020-12-18 11:17:43 +0300

    work on errors in task 5

--- LernJava/src/main/java/ru/neothat/TwoMethods.java
+++ LernJava/src/main/java/ru/neothat/TwoMethods.java
@@ -11,33 +11,52 @@ public class TwoMethods {
 
     public synchronized static void main(String[] args) {
         Arrays.fill(arr, 1);
-        inOrder();
+        measureTime(() -> inOrder(arr), "orderMethod");
         Arrays.fill(arr, 1);
-        divideAndRule();
+        measureTime(() -> divideAndRule(arr), "multiMethod");
     }
 
-    private static void divideAndRule() {
-        long a = System.currentTimeMillis();
+    private static void inOrder(float[] data){
+        inOrder(data, 0);
+    }
+
+    private static void inOrder(float[] data, int offset) {
+        for (int i = 0; i < data.length; i++) {
+            float currentValue = data[i];
+            data[i] = computerValue(i + offset, currentValue);
+        }
+    }
+
+    private static float computerValue(int index, float currentValue) {
+        return (float)(currentValue * Math.sin(0.2f + index / 5) * Math.cos(0.2f + index / 5) * Math.cos(0.4f + index / 2));
+    }
+
+    private static void divideAndRule(float[] data) {
         System.arraycopy(arr, 0, a1, 0, h);
         System.arraycopy(arr, h, a2, 0, h);
 
-        Thread firstHalf = new Thread(new FirstHalf());
-        Thread secondHalf = new Thread(new SecondHalf());
+        Thread firstHalf = new Thread(() -> inOrder(a1, 0));
+        Thread secondHalf = new Thread(() -> inOrder(a2, h));
         firstHalf.start();
         secondHalf.start();
 
+        try {
+            firstHalf.join();
+            secondHalf.join();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+            return;
+        }
+
         System.arraycopy(a1, 0, arr, 0, h);
         System.arraycopy(a2, 0, arr, h, h);
-        System.currentTimeMillis();
-        System.out.println(System.currentTimeMillis() - a);
     }
 
-    private static void inOrder() {
-        long a = System.currentTimeMillis();
-        for (int i = 0; i < arr.length; i++) {
-            arr[i] = (float) (arr[i] * Math.sin(0.2f + i / 5) * Math.cos(0.2f + i / 5) * Math.cos(0.4f + i / 2));
-        }
-        System.currentTimeMillis();
-        System.out.println(System.currentTimeMillis() - a);
+    private static void measureTime(Runnable action, String actionName) {
+        long start = System.currentTimeMillis();
+        action.run();
+        long finish = System.currentTimeMillis();
+        long duration = finish - start;
+        System.out.printf("Метод '%s' выполнился за %d ms %n", actionName, duration);
     }
 }
